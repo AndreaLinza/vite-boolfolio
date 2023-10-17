e1a9fdc0f94f99cc27d191ebd5af72ef5e4243b2
AndreaLinza <andrealinza@gmail.com>
2023-10-17 21:00:00 +0200

    store comment

--- src/store.js
+++ src/store.js
@@ -1,35 +1,39 @@
-import axios from "axios";
-import { reactive } from "vue";
 
-export const store = reactive({
-
-    projects: [],
-    pagination: {}
-});
+//////////////// STORE ANCORA DA IMPLEMENTARE ///////////////
 
 
-export function fetchData(url) {
-    axios.get(url ?? 'http://127.0.0.1:8000/api/projects')
-        .then((response) => {
-            //Andiamo a salvare la risposta in una costante
-            const results = response.data;
+// import axios from "axios";
+// import { reactive } from "vue";
 
-            //Salviamo la lista dei progetti nella variabile projects
-            store.projects = results.data;
+// export const store = reactive({
 
-            //Elimino la chiave data dall'oggetto risultati
-            delete results.data;
+//     projects: [],
+//     pagination: {}
+// });
 
-            //Salvo i dati della paginazione nella variabile pagination
-            store.pagination = results;
-            console.log(results)
-        })
-};
 
-export function getThumbUrl(){
+// export function fetchData(url) {
+//     axios.get(url ?? 'http://127.0.0.1:8000/api/projects')
+//         .then((response) => {
+//             //Andiamo a salvare la risposta in una costante
+//             const results = response.data;
+
+//             //Salviamo la lista dei progetti nella variabile projects
+//             store.projects = results.data;
+
+//             //Elimino la chiave data dall'oggetto risultati
+//             delete results.data;
+
+//             //Salvo i dati della paginazione nella variabile pagination
+//             store.pagination = results;
+//             console.log(results)
+//         })
+// };
+
+// export function getThumbUrl(){
     
     
-    return `http://127.0.0.1:8000/storage/${store.projects.thumb}`;
-}
+//     return `http://127.0.0.1:8000/storage/${store.projects.thumb}`;
+// }
 
 
